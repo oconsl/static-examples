5aad2b06af1c226a990d5891576b0bf1aedcaa97
Santiago Luis Ocón <ocon.santiago@gmail.com>
2023-04-23 23:46:44 -0300

    Add active nav-link handling on htmlTags page

---
+++ scripts/htmlTags.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const navLinks = document.querySelectorAll('.nav-link')
+
+  navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      navLinks.forEach((otherLink) => {
+        otherLink.classList.remove('active')
+      })
+      link.classList.add('active')
+    })
+  })
+})
