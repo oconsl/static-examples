2125a3699102ec7e9d3cfa1b3c69c72092d63261
Santiago Luis Ocón <ocon.santiago@gmail.com>
2023-04-24 22:45:32 -0300

    Add h4,h5 & h6 cases to the fix

--- scripts/htmlTags.js
+++ scripts/htmlTags.js
@@ -3,12 +3,18 @@ window.addEventListener('DOMContentLoaded', () => {
   const allH1 = document.querySelectorAll('h1')
   const allH2 = document.querySelectorAll('h2')
   const allH3 = document.querySelectorAll('h3')
+  const allH4 = document.querySelectorAll('h4')
+  const allH5 = document.querySelectorAll('h5')
+  const allH6 = document.querySelectorAll('h6')
   const allAnchors = document.querySelectorAll('a')
   const allButtons = document.querySelectorAll('button')
   const cancelTranslationElements = [
     ...allH1,
     ...allH2,
     ...allH3,
+    ...allH4,
+    ...allH5,
+    ...allH6,
     ...allAnchors,
     ...allButtons
   ]
