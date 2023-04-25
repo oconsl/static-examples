49fcb98a56b1652c84a0c20c2cd2ddc0964e04de
Santiago Luis Ocón <ocon.santiago@gmail.com>
2023-04-24 22:44:02 -0300

    Fix translation issue

--- scripts/htmlTags.js
+++ scripts/htmlTags.js
@@ -1,5 +1,23 @@
 window.addEventListener('DOMContentLoaded', () => {
   const navLinks = document.querySelectorAll('.nav-link')
+  const allH1 = document.querySelectorAll('h1')
+  const allH2 = document.querySelectorAll('h2')
+  const allH3 = document.querySelectorAll('h3')
+  const allAnchors = document.querySelectorAll('a')
+  const allButtons = document.querySelectorAll('button')
+  const cancelTranslationElements = [
+    ...allH1,
+    ...allH2,
+    ...allH3,
+    ...allAnchors,
+    ...allButtons
+  ]
+
+  console.log('cancelTranslationElements', cancelTranslationElements)
+
+  cancelTranslationElements.forEach((element) => {
+    element.setAttribute('translate', 'no')
+  })
 
   navLinks.forEach((link) => {
     link.addEventListener('click', () => {
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,7 @@
-window.addEventListener("DOMContentLoaded", () => {});
+window.addEventListener('DOMContentLoaded', () => {
+  const cancelTranslationElements = document.querySelectorAll('.no-translate')
+
+  cancelTranslationElements.forEach((element) => {
+    element.setAttribute('translate', 'no')
+  })
+})
